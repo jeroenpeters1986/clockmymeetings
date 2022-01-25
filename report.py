3490bfd1871f9b1b57c1641cc3de0b5594bb68b3
Jeroen Peters <jeroen.peters@yourhosting.nl>
2022-01-25 09:37:03 +0100

    oh oh oh ik heb regels van 80 chars lang en een betere crontijd

--- report.py
+++ report.py
@@ -8,6 +8,8 @@ load_dotenv()
 
 
 def get_work_week_span():
+    """ Get the start- and enddate from the current workweek as a string"""
+
     now = datetime.date.today()
     start_of_current_week = now - datetime.timedelta(days=now.weekday())
     end_of_work_week = start_of_current_week + datetime.timedelta(days=4)
@@ -17,6 +19,7 @@ def get_work_week_span():
 
 
 def clockify_time_to_object(time_string):
+    """ Convert the Clockify string (PT1H30M, PT1H, PT30M) to a time object """
     time_format = '%I:%M'
 
     if "H" in time_string and "M" not in time_string:
@@ -25,10 +28,13 @@ def clockify_time_to_object(time_string):
     if "H" not in time_string and "M" in time_string:
         time_format = '%M'
 
-    return time.strptime(time_string.replace("PT", "").replace("M", "").replace("H", ":"), time_format)
+    ftime = time_string.replace("PT", "").replace("M", "").replace("H", ":")
+
+    return time.strptime(ftime, time_format)
 
 
 def seconds_to_hours(seconds):
+    """ Convert seconds to hours """
     return datetime.timedelta(seconds=seconds)
 
 
@@ -50,7 +56,10 @@ for tag in tags:
     if tag['name'] == 'Meetings':
         meeting_tag_id = tag['id']
 
-entries = client.time_entries.get_time_entries(workspace_id, current_user['id'], {'start': workweek[0], 'end': workweek[1]})
+entries = client.time_entries.get_time_entries(
+    workspace_id,
+    current_user['id'],
+    {'start': workweek[0], 'end': workweek[1]})
 
 total_seconds = 0
 meeting_seconds = 0
@@ -63,13 +72,17 @@ for entry in entries:
 
     for tag_id in entry['tagIds']:
         if tag_id == meeting_tag_id:
-            print("  MEETING :  ", entry['description'], seconds_to_hours(total_entry_seconds))
+            print("  MEETING :  ", entry['description'],
+                  seconds_to_hours(total_entry_seconds))
             meeting_seconds += total_entry_seconds
 
     total_seconds += total_entry_seconds
 
 print("\n===============================================")
-print("Totale tijd geregistreerd:              {}".format(seconds_to_hours(total_seconds)))
-print("Totale tijd in meeting:                 {}".format(seconds_to_hours(meeting_seconds)))
-print("Percentage tijd getagd met 'Meeting':   {:.2f}%".format(100/total_seconds * meeting_seconds))
+print("Totale tijd geregistreerd:              {}".format(
+    seconds_to_hours(total_seconds)))
+print("Totale tijd in meeting:                 {}".format(
+    seconds_to_hours(meeting_seconds)))
+print("Percentage tijd getagd met 'Meeting':   {:.2f}%".format(
+    100/total_seconds * meeting_seconds))
 
